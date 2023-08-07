be3e843c7ddbb70096728cee2791aac261eee0d2
Rashadul-Islam <rashadulislam4581@gmail.com>
2023-08-07 18:42:40 +0600

    [Done] final check

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,3 @@
-import { FcGoogle } from "react-icons/fc";
 import { BsGithub } from "react-icons/bs";
 import { signIn } from "next-auth/react";
 
@@ -7,17 +6,6 @@ const LoginPage = () => {
     <div className="flex items-center justify-center h-screen">
       <div className="border w-full max-w-xs p-2 py-3 mx-2 border-gray-300 shadow-md shadow-gray-300">
         <h1 className="text-center font-semibold text-xl ">Login</h1>
-
-        <div
-          onClick={() =>
-            signIn("google", {
-              callbackUrl: "https://pc-builder-pied-three.vercel.app",
-            })
-          }
-          className="flex justify-center mt-5 border border-gray-300 hover:bg-slate-300 bg-slate-200 transition-all duration-200 rounded-full cursor-pointer "
-        >
-          <FcGoogle size={30} className="p-1" />
-        </div>
         <div
           onClick={() =>
             signIn("github", {
